42f6e91cf5eb7637d032c39f5e4fff4fe00afbad
b1-80278 <sumitshinde@outlook.com>
2023-09-10 08:26:39 +0530

    1_LeapYear updated

--- Python_Assignment_2/1_LeapYear.py
+++ Python_Assignment_2/1_LeapYear.py
@@ -2,5 +2,9 @@
 
 year = int(input("Enter year: "))
 
-if year/400 = 0:
-    if
+if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
+    print("Given year is leap year......")
+else:
+    print ("Given year is not leap year...")
+
+
