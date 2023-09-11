3be540dbfd2bd829863fda8e4b6588ddb7fea764
b1-80278 <sumitshinde@outlook.com>
2023-09-11 15:38:12 +0530

    updated all

--- Day3_Python_Assignment/2.py
+++ Day3_Python_Assignment/2.py
@@ -1,3 +1,4 @@
 # Write a program that accepts a list from user and print the alternate element of list.
 
-size = int(input(size))
+size = int(input("Enter size of list: ")
+           for
--- Day3_Python_Assignment/3.py
+++ Day3_Python_Assignment/3.py
@@ -1,3 +1,14 @@
 # Replace single element ‘b’ in given list [’a’, ’b’, ’c’, ’d’, ’e’] with [1, 2, 3].
 
 list1 = ['a', 'b', 'c', 'd', 'e']
+
+index_of_b = list1.index('b')
+
+#removing 'b' from list
+list1.pop(index_of_b)
+
+list2 = [1, 2, 3]
+
+list1.insert(index_of_b, list2)
+
+print(list1)
