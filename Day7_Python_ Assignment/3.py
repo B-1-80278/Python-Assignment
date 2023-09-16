455c0cf6b13722e24ace57e9961c71ca5601280b
b1-80278 <sumitshinde@outlook.com>
2023-09-16 16:32:35 +0530

    assignment

--- Day7_Python_ Assignment/3.py
+++ Day7_Python_ Assignment/3.py
@@ -6,6 +6,8 @@ import pandas as pd
 d1 = {'a': 100, 'b': 200, 'c':300, 'd':400, 'e':800}
 
 panda_series = pd.Series(d1.values(), index=d1.keys())
+# or
+# panda_series = pd.Series(d1)
 
 print(panda_series)
 
