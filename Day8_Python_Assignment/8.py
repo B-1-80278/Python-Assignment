2e6a3c04f4128547570c0726978c224a101aa25f
b1-80278 <sumitshinde@outlook.com>
2023-09-18 16:57:01 +0530

    assignment

--- Day8_Python_Assignment/8.py
+++ Day8_Python_Assignment/8.py
@@ -1,2 +1,18 @@
 # Find number of occurrences of each character in the string (irrespective of its case).
 
+string = "abcd efgh ijkl mnop abcd kpg"
+list1 = []
+
+for ch in string.lower():
+    if ch.isalpha():
+        list1.append(ch)
+print(list1)
+
+dict1 = dict()
+for i in list1:
+    if dict1.get(i) is None:
+        dict1[i] = 1
+    else:
+        dict1[i] +=1
+
+print(dict1)
