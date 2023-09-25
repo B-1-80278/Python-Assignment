08f883cc7ba050070ba19acc2ef61c3070964d4a
B-1-80278 <144694159+B-1-80278@users.noreply.github.com>
2023-09-25 17:33:59 +0530

    Add files via upload

---
+++ R_Assignment/6.r
@@ -0,0 +1,10 @@
+#6)Write a R program to create two 2x3 matrix and add, subtract, multiply and divide the matrices.
+
+m.1 = matrix(c(1:6), nrow = 2, ncol = 3)
+m.2 = matrix(c(7:12), nrow = 2, ncol = 3)
+print(m.1)
+print(m.2)
+print(m.1 + m.2)
+print(m.1 - m.2)
+print(m.1 * m.2)
+print(m.2 / m.1)
