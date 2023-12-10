f5cf2001ace3d57144de8a5a91f2140dca9ae2cb
B-1-80278 <144694159+B-1-80278@users.noreply.github.com>
2023-12-10 18:18:34 +0530

    Add files via upload

---
+++ JAVA Lab Assignment/Java_Assignment_1/Q2.java
@@ -0,0 +1,16 @@
+/*2. Accept 2 double values from User (using Scanner). Check data type. If
+arguments are not doubles, supply suitable error message & terminate.
+If numbers are double values, print its average.*/
+
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st double value: ");
+        double d1 = sc.nextDouble();
+        System.out.println("Enter 2nd double value: ");
+        double d2 = sc.nextDouble();
+
+    }
+}
